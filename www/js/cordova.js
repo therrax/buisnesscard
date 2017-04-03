4ea47ad2557157edfbeba4f8d7bc0d368e165ae9
Jerzy Kołakowski <therrax@gmail.com>
2017-04-03 22:49:40 +0200

    changes

---
+++ www/js/cordova.js
@@ -0,0 +1,62 @@
+function init() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+    document.addEventListener("online", onOnline, false);
+    document.addEventListener("offline", onOffline, false);
+}
+function onDeviceReady() {
+    navigator.notification.beep(1);
+}
+function deviceInfo() {
+    info = 'Hi, I am your smartphone :-)' +
+        '\n' + '=====' + '\n' + 'Device Name    : ' + device.name +
+        '\n' + 'Device Cordova : ' + device.cordova +
+        '\n' + 'Device Platform: ' + device.platform +
+        '\n' + 'Device UUID    : ' + device.uuid +
+        '\n' + 'Device Model   : ' + device.model +
+        '\n' + 'Device Version : ' + device.version +
+        '\n'; navigator.notification.alert(info);
+}
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN] = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI] = 'WiFi connection';
+    states[Connection.CELL_2G] = 'Cell 2G connection';
+    states[Connection.CELL_3G] = 'Cell 3G connection';
+    states[Connection.CELL_4G] = 'Cell 4G connection';
+    states[Connection.CELL] = 'Cell generic connection';
+    states[Connection.NONE] = 'No network connection';
+
+    alert('Connection type: ' + states[networkState]);
+}
+function onOnline() {
+    navigator.notification.alert("Online");
+}
+function onOffline() {
+    navigator.notification.alert("Offline");
+}
+function onSuccess(acceleration) {
+    var accelerometer = document.querySelector("#accelerometer")
+    accelerometer.text = 'Acceleration X: ' + acceleration.x + '<br/>'
+          +
+          'Acceleration Y: ' + acceleration.y + '<br/>'
+          +
+          'Acceleration Z: ' + acceleration.z + '<br/>'
+          +
+          'Timestamp: ' + acceleration.timestamp + '<br/>'
+        ;
+}
+
+function onError() {
+    alert('onError!');
+}
+function startA(){
+var options = { frequency: 500 };  // Update every 3 seconds
+
+var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+}
+function stopA() {
+    navigator.accelerometer.clearWatch(watchID);
+}
