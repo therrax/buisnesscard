c6483429fb7d68f63f6b754634e960f1f435295d
Jerzy Kołakowski <therrax@gmail.com>
2017-04-04 01:50:07 +0200

    Sms

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,4 +1,4 @@
-let watchID = null;
+﻿let watchID = null;
 function init() {
     document.addEventListener("deviceready", onDeviceReady, false);
     document.addEventListener("online", onOnline, false);
@@ -6,9 +6,9 @@ function init() {
 
 }
 function onDeviceReady() {
-   // navigator.notification.beep(1);
+    // navigator.notification.beep(1);
 
-   
+
 }
 function startAcc() {
     var options = { frequency: 3000 };  // Update every 3 seconds
@@ -53,7 +53,7 @@ function onSuccess(acceleration) {
 
     var p = document.querySelector('#accText');
 
-    p.innerHTML='Acceleration X: ' +
+    p.innerHTML = 'Acceleration X: ' +
         acceleration.x +
         '<br/>' +
         'Acceleration Y: ' +
@@ -70,17 +70,19 @@ function onSuccess(acceleration) {
 function onError() {
     navigator.notification.alert('onError!');
 }
+// Splashscreen
 function showSplash() {
     navigator.splashscreen.show();
     setTimeout(function () {
         navigator.splashscreen.hide();
     }, 2000);
 }
-
+//End of splashscreen
+// Contacts
 function Contacts(contacts) {
-    let list;
+    let list = "";
     for (var i = 0; i < contacts.length; i++) {
-        list += "Nazwa: " + contacts[i].displayName + " - " + contacts[i].phoneNumbers;
+        list += "Nazwa: " + contacts[i].displayName + " - " + contacts[i].phoneNumber + "\n";
     }
     navigator.notification.alert(list)
 };
@@ -89,10 +91,34 @@ function onErrorC(contactError) {
     navigator.notification.alert('onError!');
 };
 function showContacts() {
- // find all contacts
-var optionsC = new ContactFindOptions();
-optionsC.filter = "";
-optionsC.multiple = true;
-var filter = ["displayName", "phoneNumbers"];
-navigator.contacts.find(filter, Contacts, onErrorC, optionsC);   
+    var optionsC = new ContactFindOptions();
+    optionsC.filter = "";
+    optionsC.multiple = true;
+    var filter = ["displayName", "phoneNumbers"];
+    navigator.contacts.find(filter, Contacts, onErrorC, optionsC);
 }
+
+function sendMessage() {
+    var message = document.querySelector("#message")
+
+}
+var app = {
+    sendSms: function () {
+        var number = document.getElementById('numberTxt').value;
+        var message = document.getElementById('messageTxt').value;
+        
+
+        //CONFIGURATION
+        var options = {
+            replaceLineBreaks: false, // true to replace \n by a new line, false by default
+            android: {
+                intent: 'INTENT'  // send SMS with the native android SMS messaging
+                //intent: '' // send SMS without open any other app
+            }
+        };
+
+        var success = function () { alert('Wiadomość została wysłana'); };
+        var error = function (e) { alert('Błąd: ' + e); };
+        sms.send(number, message, options, success, error);
+    }
+};
