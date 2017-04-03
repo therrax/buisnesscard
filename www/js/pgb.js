dd81ef46371f3921fc8f304880ba0295e88ac2fd
Jerzy Kołakowski <therrax@gmail.com>
2017-04-04 00:33:54 +0200

    Fixes

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,8 +1,22 @@
+var watchID = null;
 function init() {
     document.addEventListener("deviceready", onDeviceReady, false);
+    document.addEventListener("online", onOnline, false);
+    document.addEventListener("offline", onOffline, false);
+
 }
 function onDeviceReady() {
-    navigator.notification.beep(1);
+   // navigator.notification.beep(1);
+
+   
+}
+function startAcc() {
+    var options = { frequency: 3000 };  // Update every 3 seconds
+
+    watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+}
+function stopAcc() {
+    navigator.accelerometer.clearWatch(watchID);
 }
 function deviceInfo() {
     info = 'Hi, I am your smartphone :-)' +
@@ -13,4 +27,46 @@ function deviceInfo() {
         '\n' + 'Device Model   : ' + device.model +
         '\n' + 'Device Version : ' + device.version +
         '\n'; navigator.notification.alert(info);
+}
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN] = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI] = 'WiFi connection';
+    states[Connection.CELL_2G] = 'Cell 2G connection';
+    states[Connection.CELL_3G] = 'Cell 3G connection';
+    states[Connection.CELL_4G] = 'Cell 4G connection';
+    states[Connection.CELL] = 'Cell generic connection';
+    states[Connection.NONE] = 'No network connection';
+
+    navigator.notification.alert('Connection type: ' + states[networkState]);
+}
+function onOnline() {
+    navigator.notification.alert("Online");
+}
+function onOffline() {
+    navigator.notification.alert("Offline");
+}
+function onSuccess(acceleration) {
+
+    var p = document.querySelector('#accText');
+
+    p.text('Acceleration X: ' +
+        acceleration.x +
+        '\n' +
+        'Acceleration Y: ' +
+        acceleration.y +
+        '\n' +
+        'Acceleration Z: ' +
+        acceleration.z +
+        '\n' +
+        'Timestamp: ' +
+        acceleration.timestamp +
+        '\n');
+}
+
+function onError() {
+    navigator.notification.alert('onError!');
 }
